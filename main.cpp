fe2775de990b3841a42514ec3648b4c640b12482
RekhalovAS <rekhalovas@yandex.ru>
2022-04-13 22:45:20 +0300

    before decomposition

--- main.cpp
+++ main.cpp
@@ -1,78 +1,116 @@
 #include <iostream>
 #include <vector>
-
+#include <math.h>
+#include <locale.h>
 using namespace std;
 
-int
-main() {
-    // Ввод данных
+vector<double> input_numbers(size_t count) //функция ввода чисел
+{
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++)
+    {
+        cin >> result[i];
+    }
+    return result;
+}
+
+void find_minmax(const vector<double>& numbers, double& min, double& max) //функция поиска минимума и макс
+{
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+}
+
+vector<size_t> make_histogram (vector<double>numbers, size_t bin_count, double min, double max)
+{
+    vector<size_t> bins(bin_count);
+    for (double number : numbers)
+    {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count)
+        {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+
+void show_histogram_text(vector<size_t> bins)
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
     size_t number_count;
-    cerr << "Enter number count: ";
+    cout << "Enter number count: ";
     cin >> number_count;
 
-    cerr << "Enter numbers: ";
-    vector<double> numbers(number_count);
-    for (size_t i = 0; i < number_count; i++) {
-        cin >> numbers[i];
-    }
+    cout << "Enter numbers: ";
+
+    // Ввод чисел заменен вызовом функции:
+    const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
 
     // Обработка данных
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
+    double min, max;
+    find_minmax(numbers,min,max);
 
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        bins[bin]++;
-    }
+    const auto bins = make_histogram(numbers, bin_count, min, max);
 
     // Вывод данных
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-
-    size_t max_count = 0;
-    for (size_t count : bins) {
-        if (count > max_count) {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins) {
-        if (bin < 100) {
-            cout << ' ';
-        }
-        if (bin < 10) {
-            cout << ' ';
-        }
-        cout << bin << "|";
-
-        size_t height = bin;
-        if (scaling_needed) {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-        }
-
-        for (size_t i = 0; i < height; i++) {
-            cout << '*';
-        }
-        cout << '\n';
-    }
+    show_histogram_text(bins);
 
     return 0;
 }
